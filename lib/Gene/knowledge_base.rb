07f903fc3fa2bd22ed45026c12a4a3f04823d45d
Miguel Vazquez <mikisvaz@gmail.com>
2026-07-15 08:25:57 +0200

    Update CollecTRI2 registry

--- lib/Gene/knowledge_base.rb
+++ lib/Gene/knowledge_base.rb
@@ -5,5 +5,3 @@ module Gene
   self.knowledge_base = KnowledgeBase.new Rbbt.var.Gene.knowledge_base
   #self.knowledge_base.register :signor, Signor.protein_protein, source: "=>Associated Gene Name", target: "=>Associated Gene Name", identifiers: Organism.identifiers("NAMESPACE"), namespace: "Hsa/feb2014"
 end
-
-
